a7a0c0ad6b264a871c546d3ec007dba6c17e8fbb
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-14 12:51:29 -0800

    update: export from index

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,26 @@
 import Application from "./application";
+import { ApplicationMixin, applicationMixin } from "./applicationMixin";
+import ApplicationRegistry from "./ApplicationRegistry";
+import ContextProvider from "./context/ContextProvider";
+import JSONSchemaFormDataProvider from "./context/JSONSchemaFormDataProvider";
 import Executable from "./executable";
+import { ExecutableMixin, executableMixin } from "./executableMixin";
 import Flavor from "./flavor";
+import { FlavorMixin, flavorMixin } from "./flavorMixin";
 import Template from "./template";
+import { TemplateMixin, templateMixin } from "./templateMixin";
 
-export { Application, Executable, Flavor, Template };
+export {
+    Application,
+    Executable,
+    Flavor,
+    Template,
+    ApplicationRegistry,
+    ContextProvider,
+    JSONSchemaFormDataProvider,
+    executableMixin,
+    flavorMixin,
+    applicationMixin,
+    templateMixin,
+};
+export type { FlavorMixin, ExecutableMixin, ApplicationMixin, TemplateMixin };
