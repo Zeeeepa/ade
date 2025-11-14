34ba9839acef89c167514a13a8708071e1f462b2
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-14 13:34:52 -0800

    udpate: export from index

--- dist/js/index.d.ts
+++ dist/js/index.d.ts
@@ -1,5 +1,13 @@
 import Application from "./application";
+import { ApplicationMixin, applicationMixin } from "./applicationMixin";
+import ApplicationRegistry from "./ApplicationRegistry";
+import ContextProvider from "./context/ContextProvider";
+import JSONSchemaFormDataProvider from "./context/JSONSchemaFormDataProvider";
 import Executable from "./executable";
+import { ExecutableMixin, executableMixin } from "./executableMixin";
 import Flavor from "./flavor";
+import { FlavorMixin, flavorMixin } from "./flavorMixin";
 import Template from "./template";
-export { Application, Executable, Flavor, Template };
+import { TemplateMixin, templateMixin } from "./templateMixin";
+export { Application, Executable, Flavor, Template, ApplicationRegistry, ContextProvider, JSONSchemaFormDataProvider, executableMixin, flavorMixin, applicationMixin, templateMixin, };
+export type { FlavorMixin, ExecutableMixin, ApplicationMixin, TemplateMixin };
